234a0dfdfc49f69e75cc948e8ebec9a0500348f5
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-20 15:37:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const items = ["cleanser", "mask", "scrub", "sunscreen", "moisturizer"];
+
+function showItems(items) {
+  document.querySelector("#products").innerHTML = "";
+  items.forEach((items) => {
+    document.querySelector("#products").innerHTML += `<li>${items}</li>`;
+  });
+}
+showItems(items);
+
+function addItems() {
+  const item = document.querySelector("#addProduct").value;
+  if (item === "")
+    document.querySelector("#products").innerHTML = `ENTER VALID VALUE`;
+  else {
+    items.push(item);
+    showItems(items);
+  }
+}
+
+function findItems() {
+  const text = document.querySelector("#findProduct").value;
+  const item = items.find((search) => search.includes(text));
+  document.querySelector("#products").innerHTML += `<li>${
+    items ? items : "no matches"
+  }</li>`;
+}
